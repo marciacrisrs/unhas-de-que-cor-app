d9bedf60d7e14beda6eccb70f11cdb95707b8ebd
Marcia <57639202+marciacrisrs@users.noreply.github.com>
2026-08-16 19:00:08 -0300

    test(a11y): remove unused matcher import

--- app/src/androidTest/java/br/com/unhasdequecor/ui/components/AsyncContentAccessibilityTest.kt
+++ app/src/androidTest/java/br/com/unhasdequecor/ui/components/AsyncContentAccessibilityTest.kt
@@ -4,7 +4,6 @@ import androidx.compose.ui.semantics.LiveRegionMode
 import androidx.compose.ui.semantics.SemanticsProperties
 import androidx.compose.ui.test.SemanticsMatcher
 import androidx.compose.ui.test.assert
-import androidx.compose.ui.test.assertContentDescriptionContains
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithContentDescription
 import androidx.compose.ui.test.onNodeWithText
