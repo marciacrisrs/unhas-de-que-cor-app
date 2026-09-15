79af7c530e91bb84badae8e7509c886e3ca0bb4d
Marcia <57639202+marciacrisrs@users.noreply.github.com>
2026-09-14 23:27:29 -0300

    test: remove unused MockK argument helpers

--- app/src/test/java/br/com/unhasdequecor/data/vision/nail/MockKIndexedArgs.kt
+++
@@ -1,9 +0,0 @@
-package br.com.unhasdequecor.data.vision.nail
-
-import io.mockk.MockKAnswerScope
-
-/** MockK 1.14 only ships first/second/thirdArg; Bitmap.getPixels stubs need the rest. */
-internal inline fun <reified T : Any> MockKAnswerScope<*, *>.fourthArg(): T = invocation.args[3] as T
-internal inline fun <reified T : Any> MockKAnswerScope<*, *>.fifthArg(): T = invocation.args[4] as T
-internal inline fun <reified T : Any> MockKAnswerScope<*, *>.sixthArg(): T = invocation.args[5] as T
-internal inline fun <reified T : Any> MockKAnswerScope<*, *>.seventhArg(): T = invocation.args[6] as T
